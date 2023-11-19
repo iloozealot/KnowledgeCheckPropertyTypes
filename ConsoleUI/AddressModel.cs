ffd30084d1f9caa88b85c58e6bf8842394b1e547
iloozealot <iloozealot@gmail.com>
2023-11-19 16:50:58 -0600

    Add project files.

---
+++ ConsoleUI/AddressModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class AddressModel
+    {
+        public int StreetNumber {  get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+
+        public string FullAddress
+        {
+            get
+            {
+                return $"{StreetNumber} {StreetName} {City}, {State} {PostalCode}.";
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using ConsoleUI;
+
+AddressModel address = new AddressModel();
+
+address.StreetNumber = 12007;
+address.StreetName = "W Cora St";
+address.City = "Wichita";
+address.State = "KS";
+address.PostalCode = 67205;
+
+Console.WriteLine(address.FullAddress);
+
+Console.ReadLine();
